1c130687271c46f9b1cfd7815c7693772becc1cb
kobbyob123 <popolapii@gmail.com>
2022-12-04 13:18:34 +0000

    Adjusted to pycodestyle (PEP8)

--- largest_number_bsa.py
+++ largest_number_bsa.py
@@ -1,5 +1,5 @@
-### This is a program to find the largest number in a list
-### This involves the use of the bubble sort algorithm
+# This is a program to find the largest number in a list
+# This involves the use of the bubble sort algorithm
 
 def largest(*numbers):
     """Returns the largest number in the list."""
@@ -7,11 +7,12 @@ def largest(*numbers):
     for number in numbers:
         group.append(number)
     n = len(group)
-    #group.sort()
+    # group.sort()
     # How to rearrange the numbers in the list without the sort() function
-    for x in range(0,n):
-        for y in range(0,n):
+    for x in range(0, n):
+        for y in range(0, n):
             if group[x] < group[y]:
-                # less than arranges it in ascending order, greater than for descending order
+                '''less than arranges it in ascending order,
+                   greater than for descending order'''
                 group[x], group[y] = group[y], group[x]
     return group[n-1]
