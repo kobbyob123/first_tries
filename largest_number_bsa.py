be2c56e28f3b731743df79db4d7195e8d6b63fb4
Kwabena Obeng <93714286+kobbyob123@users.noreply.github.com>
2026-02-19 00:16:39 +0100

    updated the variable names and made the code cleaner to read and also added an example on how to use the code.

--- largest_number_bsa.py
+++ largest_number_bsa.py
@@ -1,18 +1,34 @@
-# This is a program to find the largest number in a list
-# This involves the use of the bubble sort algorithm
+# largest_number_bsa.py
+# This is an implementation of the Bubble Sort Algortihm for small-sized lists
 
 def largest(*numbers):
     """Returns the largest number in the list."""
-    group = []
+    
+    numbers_list = []
+    
+    # Storing the numbers in a list
     for number in numbers:
-        group.append(number)
-    n = len(group)
-    # group.sort()
+        numbers_list.append(number)
+    
+    # Get the length of this list
+    n = len(numbers_list)
+    
     # How to rearrange the numbers in the list without the sort() function
     for x in range(0, n):
         for y in range(0, n):
-            if group[x] < group[y]:
+            if numbers_list[x] < numbers_list[y]:
                 '''less than arranges it in ascending order,
                    greater than for descending order'''
-                group[x], group[y] = group[y], group[x]
-    return group[n-1]
+                numbers_list[x], numbers_list[y] = numbers_list[y], numbers_list[x]
+    
+    # numbers_list.sort()
+    
+    # returns the largest number in the list
+    return numbers_list[-1]
+
+# Using function
+from random import randint as rdint
+nls = [rdint(0, 100) for x in range(10)]
+bigOne = largest(nls)
+
+print(str(nls) + " --> " + str(bigOne))
